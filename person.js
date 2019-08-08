d94acbd1aec29b394d7343097b42880133f88016
Etienne Laramee <elaramee@biointelligence.com>
2019-08-08 17:21:01 -0400

    Completed the tutorial

---
+++ person.js
@@ -0,0 +1,12 @@
+class person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age} years old`);
+    }
+}
+
+module.exports = person;
---
+++ reference/url_demo.js
@@ -0,0 +1,30 @@
+const url = require('url');
+
+const myUrl = new URL(
+    'http://etiennelaramee.com/hello.html?id=100&status=active'
+);
+
+//Serialize URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
+
+// Host (root domain)
+console.log(myUrl.host);
+
+// Host name (does not get port)
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialize query
+console.log(myUrl.search);
+
+// Add param
+myUrl.searchParams.append('abc', '123');
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Loop through params
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`));
